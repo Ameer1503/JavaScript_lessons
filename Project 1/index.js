a8c09a82352db4cb712c9ba9760ef1912a82ff48
Ameer Adeigbe <ameeradeigbe@gmail.com>
2023-02-27 02:28:32 -0800

    completed project 1 or day 1

--- Project 1/index.js
+++ Project 1/index.js
@@ -9,9 +9,12 @@ function decreaser(){
     if (count <= 0)
     alert('There is no one in the room')
     else
-    count = count - 1
+    count += - 1
     document.getElementById("counter").innerText = count
-
-    
-    
+}
+function savebutton(){
+   let counter_save = count + " - "
+   document.getElementById("count-state"). textContent += counter_save
+   document.getElementById("counter").textContent = 0
+   count = 0
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-TEaching myself JavaScript
+Teaching myself JavaScript
+
+Project 1 is a counting app;
+	i. it initializes at "0"
+	ii. can increate to infinity
+	iii. can decrease to "0" if less than zero it returns an alert message
+	iv. it can also save state of an initial count the initializes the counter back to "0"
